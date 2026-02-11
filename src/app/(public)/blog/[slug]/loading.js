af65391e2c41c74536fbcf4f521f61011d16f4d6
yashkesariya <yashkesariya24@gmail.com>
2026-02-11 09:26:14 +0530

    added loader in blog and dynamic blog

--- src/app/(public)/blog/[slug]/loading.js
+++ src/app/(public)/blog/[slug]/loading.js
@@ -1,14 +1,7 @@
+import Loader from "@/components/loader";
+
 export default function Loading() {
     return (
-        <div
-            style={{
-                padding: "40px",
-                backgroundColor: "#020617",
-                color: "#facc15",
-                fontSize: "20px",
-            }}
-        >
-            Loading blog post...
-        </div>
+        <Loader />
     );
 }
--- src/app/(public)/blog/loading.js
+++ src/app/(public)/blog/loading.js
@@ -1,7 +1,9 @@
+import Loader from "@/components/loader";
+
 export default function Loading() {
     return (
-        <div style={{ padding: "30px", color: "#fa1515", fontSize: "20px" }}>
-            Loading blog page...
+        <div>
+            <Loader />
         </div>
     );
 }
