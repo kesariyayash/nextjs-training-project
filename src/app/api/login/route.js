188c1b0fb12e9281fbddd755caf6c1888433693c
yashkesariya <yashkesariya24@gmail.com>
2026-02-13 18:47:16 +0530

    re-configured cookie set schema

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -51,8 +51,8 @@ export async function POST(request) {
 
         response.cookies.set("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "lax",
+            secure: "true",
+            sameSite: "none",
             path: "/",
             maxAge: 60 * 60 * 24 * 7 // 7 days
         });
